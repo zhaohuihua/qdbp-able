4950950c6e58a66d9630c3c33334b3213cf91f5e
zhaohuihua <zhaohuihua@126.com>
2019-06-22 17:35:10 +0800

    修改包路径

---
+++ able/src/main/java/com/gitee/qdbp/able/jdbc/fields/Fields.java
@@ -0,0 +1,14 @@
+package com.gitee.qdbp.able.jdbc.fields;
+
+import java.util.List;
+
+/**
+ * 字段容器
+ *
+ * @author zhaohuihua
+ * @version 180503
+ */
+public interface Fields {
+
+    List<String> getItems();
+}
