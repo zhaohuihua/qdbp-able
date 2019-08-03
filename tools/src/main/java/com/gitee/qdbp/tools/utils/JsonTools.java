565820ef54f37c5409a6a97ff94256f6d79e703e
zhaohuihua <zhaohuihua@126.com>
2019-08-03 10:45:54 +0800

    JsonTools.beanToMap不能直接用fastjson的JSON.toJSON转换, 会导致日期/枚举被转换为基本类型

--- tools/src/main/java/com/gitee/qdbp/tools/utils/JsonTools.java
+++ tools/src/main/java/com/gitee/qdbp/tools/utils/JsonTools.java
@@ -1,14 +1,24 @@
 package com.gitee.qdbp.tools.utils;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONException;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.serializer.DoubleSerializer;
+import com.alibaba.fastjson.serializer.JSONSerializable;
 import com.alibaba.fastjson.serializer.JSONSerializer;
+import com.alibaba.fastjson.serializer.JavaBeanSerializer;
+import com.alibaba.fastjson.serializer.ObjectSerializer;
 import com.alibaba.fastjson.serializer.SerializeConfig;
 import com.alibaba.fastjson.serializer.SerializeWriter;
 import com.alibaba.fastjson.serializer.SerializerFeature;
@@ -78,13 +88,135 @@ public abstract class JsonTools {
     }
 
     /**
-     * 将Java对象转换为Map
+     * 将Java对象转换为Map<br>
+     * copy from fastjson JSON.toJSON(), 保留enum和date
      * 
-     * @param object Java对象
+     * @param bean JavaBean对象
      * @return Map
      */
-    public static Map<String, Object> beanToMap(Object object) {
-        return beanToMap(object, false);
+    public static Map<String, Object> beanToMap(Object bean) {
+        if (bean == null) {
+            return null;
+        }
+
+        Object json = beanToJson(bean, SerializeConfig.globalInstance);
+        if (json instanceof JSONObject) {
+            return (JSONObject) json;
+        } else {
+            throw new IllegalArgumentException(bean.getClass().getSimpleName() + " can't convert to map.");
+        }
+    }
+
+    private static Object beanToJson(Object bean, SerializeConfig config) {
+        if (bean == null) {
+            return null;
+        }
+
+        if (bean instanceof JSON) {
+            return bean;
+        }
+
+        if (bean instanceof Map) {
+            Map<?, ?> map = (Map<?, ?>) bean;
+
+            Map<String, Object> innerMap;
+            if (map instanceof LinkedHashMap) {
+                innerMap = new LinkedHashMap<>(map.size());
+            } else if (map instanceof TreeMap) {
+                innerMap = new TreeMap<>();
+            } else {
+                innerMap = new HashMap<>(map.size());
+            }
+
+            JSONObject json = new JSONObject(innerMap);
+
+            for (Map.Entry<?, ?> entry : map.entrySet()) {
+                String jsonKey = TypeUtils.castToString(entry.getKey());
+                Object jsonValue = beanToMap(entry.getValue());
+                json.put(jsonKey, jsonValue);
+            }
+
+            return json;
+        }
+
+        if (bean instanceof Collection) {
+            Collection<?> collection = (Collection<?>) bean;
+
+            JSONArray array = new JSONArray(collection.size());
+
+            for (Object item : collection) {
+                Object jsonValue = beanToJson(item, config);
+                array.add(jsonValue);
+            }
+
+            return array;
+        }
+
+        if (bean instanceof JSONSerializable) {
+            String json = JSON.toJSONString(bean);
+            return JSON.parse(json);
+        }
+
+        Class<?> clazz = bean.getClass();
+
+        if (clazz.isEnum()) {
+            // return ((Enum<?>) bean).name();
+            return bean;
+        }
+        if (clazz == String.class) {
+            return bean;
+        }
+        if (CharSequence.class.isAssignableFrom(clazz)) {
+            return bean.toString();
+        }
+        if (isPrimitive(clazz)) {
+            return bean;
+        }
+
+        if (clazz.isArray()) {
+            int len = Array.getLength(bean);
+
+            JSONArray array = new JSONArray(len);
+
+            for (int i = 0; i < len; ++i) {
+                Object item = Array.get(bean, i);
+                Object jsonValue = beanToJson(item, config);
+                array.add(jsonValue);
+            }
+
+            return array;
+        }
+
+        ObjectSerializer serializer = config.getObjectWriter(clazz);
+        if (serializer instanceof JavaBeanSerializer) {
+            JavaBeanSerializer javaBeanSerializer = (JavaBeanSerializer) serializer;
+
+            JSONObject json = new JSONObject();
+            try {
+                Map<String, Object> values = javaBeanSerializer.getFieldValuesMap(bean);
+                for (Map.Entry<String, Object> entry : values.entrySet()) {
+                    json.put(entry.getKey(), beanToJson(entry.getValue(), config));
+                }
+            } catch (Exception e) {
+                throw new JSONException("BeanConvertToJsonError", e);
+            }
+            return json;
+        }
+
+        String text = JSON.toJSONString(bean);
+        return JSON.parse(text);
+    }
+
+    private static boolean isPrimitive(Class<?> clazz) {
+        // @formatter:off
+        return clazz.isPrimitive()
+            || clazz.isEnum()
+            || clazz == Boolean.class
+            || clazz == Character.class
+            || clazz == String.class
+            || Number.class.isAssignableFrom(clazz)
+            || Date.class.isAssignableFrom(clazz);
+        // @formatter:on
     }
 
     /**
