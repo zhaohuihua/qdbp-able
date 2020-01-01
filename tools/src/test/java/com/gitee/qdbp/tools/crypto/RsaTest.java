e015e5a925254b5aa042254512daee714bcc4e78
zhaohuihua <zhaohuihua@126.com>
2020-01-01 20:56:08 +0800

    RSA Test

---
+++ tools/src/test/java/com/gitee/qdbp/tools/crypto/RsaTest.java
@@ -0,0 +1,19 @@
+package com.gitee.qdbp.tools.crypto;
+
+public class RsaTest {
+
+    public static void main(String[] args) {
+        String string = "RSA 加密解密 测试";
+        testCipher(string);
+    }
+
+    protected static void testCipher(String string) {
+        RsaCipher rsaCipher = new RsaCipher(512);
+        System.out.println("公钥: " + rsaCipher.getPublicKey());
+        String ciphertext = rsaCipher.encrypt(string);
+        System.out.println("加密: " + ciphertext);
+        // A用B的公钥+自己的私钥解密
+        String output = rsaCipher.decrypt(ciphertext);
+        System.out.println("解密: " + output);
+    }
+}
