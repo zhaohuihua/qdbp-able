4a18093ea4e3cac9ed9b04dda39d00538067f340
zhaohuihua <zhaohuihua@126.com>
2020-01-29 22:42:33 +0800

    增加DbFieldName/DbFieldValue

---
+++ able/src/main/java/com/gitee/qdbp/able/jdbc/model/DbFieldValue.java
@@ -0,0 +1,34 @@
+package com.gitee.qdbp.able.jdbc.model;
+
+import java.io.Serializable;
+
+/**
+ * 字段值<br>
+ * 自定义对象保存到数据库时需要转换(转换目标一般是Boolean/Character/Date/Number/String之一)<br>
+ * 先定义一个转换类: XxxToDbValueConverter implements Converter&lt;XxxEntity, DbFieldValue&gr;<br>
+ * 再注入spring的ConversionService之中, ConfigableDataConvertHandler会判断XxxEntity能否转换为DbFieldValue
+ *
+ * @author zhaohuihua
+ * @version 200123
+ */
+public class DbFieldValue implements Serializable {
+
+    /** SerialVersionUID **/
+    private static final long serialVersionUID = 1L;
+    private Object fieldValue;
+
+    public DbFieldValue() {
+    }
+
+    public DbFieldValue(Object fieldValue) {
+        this.fieldValue = fieldValue;
+    }
+
+    public Object getFieldValue() {
+        return fieldValue;
+    }
+
+    public void setFieldValue(String fieldValue) {
+        this.fieldValue = fieldValue;
+    }
+}
