5c146b136db9776f98c0e968b6f954a5ef1b959f
zhaohuihua <zhaohuihua@126.com>
2020-02-02 17:41:19 +0800

    EnumInterface枚举接口

---
+++ able/src/main/java/com/gitee/qdbp/able/enums/EnumInterface.java
@@ -0,0 +1,17 @@
+package com.gitee.qdbp.able.enums;
+
+/**
+ * 枚举接口<br>
+ * 某些枚举, 其枚举值随着项目的不同而有差异<br>
+ * 但基础项目定义的类不能因业务项目的差异化而修改代码<br>
+ * 因此在基础项目中将枚举改为接口, 由各业务项目提供实际的枚举类
+ *
+ * @author zhaohuihua
+ * @version 200202
+ */
+public interface EnumInterface {
+
+    int ordinal();
+
+    String name();
+}
