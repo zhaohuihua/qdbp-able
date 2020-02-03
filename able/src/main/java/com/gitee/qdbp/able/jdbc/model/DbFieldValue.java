24d0bc5ee9b4ff6d262904cacf825dcb97f104aa
zhaohuihua <zhaohuihua@126.com>
2020-02-03 23:17:34 +0800

    修改注释

--- able/src/main/java/com/gitee/qdbp/able/jdbc/model/DbFieldValue.java
+++ able/src/main/java/com/gitee/qdbp/able/jdbc/model/DbFieldValue.java
@@ -5,8 +5,8 @@ import java.io.Serializable;
 /**
  * 字段值<br>
  * 自定义对象保存到数据库时需要转换(转换目标一般是Boolean/Character/Date/Number/String之一)<br>
- * 先定义一个转换类: XxxToDbValueConverter implements Converter&lt;XxxEntity, DbFieldValue&gr;<br>
- * 再注入spring的ConversionService之中, ConfigableDataConvertHandler会判断XxxEntity能否转换为DbFieldValue
+ * 先定义一个转换类: XxxToDbValueConverter implements Converter或GenericConverter<br>
+ * 再注入spring的ConversionService之中, SpringVarToDbValueConverter会判断XxxEntity能否转换为DbFieldValue
  *
  * @author zhaohuihua
  * @version 200123
