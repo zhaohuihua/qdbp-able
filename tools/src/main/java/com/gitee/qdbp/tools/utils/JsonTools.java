3b9551738dda417e1b888ff7419f27d7caa147ee
zhaohuihua <zhaohuihua@126.com>
2020-02-07 21:02:13 +0800

    JsonTools工具优化

--- tools/src/main/java/com/gitee/qdbp/tools/utils/JsonTools.java
+++ tools/src/main/java/com/gitee/qdbp/tools/utils/JsonTools.java
@@ -113,13 +113,110 @@ public abstract class JsonTools {
         if (bean == null) {
             return null;
         }
+        return beanToMap(bean, true, true);
+    }
 
-        Object json = beanToJson(bean, SerializeConfig.getGlobalInstance());
-        if (json instanceof JSONObject) {
-            return (JSONObject) json;
-        } else {
+    /**
+     * 将Java对象转换为Map
+     * 
+     * @param object Java对象
+     * @param deep 是否递归转换子对象
+     * @param clearBlankValue 是否清除空值
+     * @return Map
+     */
+    public static Map<String, Object> beanToMap(Object object, boolean deep, boolean clearBlankValue) {
+        if (object == null) {
+            return null;
+        }
+        Map<String, Object> map = getBeanFieldValuesMap(object, deep, SerializeConfig.getGlobalInstance());
+        if (clearBlankValue) {
+            ConvertTools.clearBlankValue(map);
+        }
+        return map;
+    }
+
+    protected static JSONObject getBeanFieldValuesMap(Object bean, boolean deep, SerializeConfig config) {
+        if (bean == null) {
+            return null;
+        }
+
+        if (bean instanceof JSONObject) {
+            return (JSONObject) bean;
+        }
+
+        if (bean instanceof JSONArray) {
             throw new IllegalArgumentException(bean.getClass().getSimpleName() + " can't convert to map.");
         }
+
+        if (bean instanceof Map) {
+            Map<?, ?> map = (Map<?, ?>) bean;
+
+            Map<String, Object> innerMap;
+            if (map instanceof LinkedHashMap) {
+                innerMap = new LinkedHashMap<>(map.size());
+            } else if (map instanceof TreeMap) {
+                innerMap = new TreeMap<>();
+            } else {
+                innerMap = new HashMap<>(map.size());
+            }
+
+            JSONObject json = new JSONObject(innerMap);
+
+            for (Map.Entry<?, ?> entry : map.entrySet()) {
+                String jsonKey = TypeUtils.castToString(entry.getKey());
+                Object jsonValue = !deep ? entry.getValue() : beanToJson(entry.getValue(), config);
+                json.put(jsonKey, jsonValue);
+            }
+
+            return json;
+        }
+
+        if (bean instanceof Collection) {
+            throw new IllegalArgumentException(bean.getClass().getSimpleName() + " can't convert to map.");
+        }
+
+        if (bean instanceof JSONSerializable) {
+            String string = JSON.toJSONString(bean);
+            Object json = JSON.parse(string);
+            return getBeanFieldValuesMap(json, deep, config);
+        }
+
+        Class<?> clazz = bean.getClass();
+
+        if (clazz.isEnum()) {
+            throw new IllegalArgumentException(bean.getClass().getSimpleName() + " can't convert to map.");
+        }
+        if (clazz == String.class) {
+            throw new IllegalArgumentException(bean.getClass().getSimpleName() + " can't convert to map.");
+        }
+        if (CharSequence.class.isAssignableFrom(clazz)) {
+            throw new IllegalArgumentException(bean.getClass().getSimpleName() + " can't convert to map.");
+        }
+        if (isPrimitive(clazz)) {
+            throw new IllegalArgumentException(bean.getClass().getSimpleName() + " can't convert to map.");
+        }
+        if (clazz.isArray()) {
+            throw new IllegalArgumentException(bean.getClass().getSimpleName() + " can't convert to map.");
+        }
+
+        ObjectSerializer serializer = config.getObjectWriter(clazz);
+        if (serializer instanceof JavaBeanSerializer) {
+            JavaBeanSerializer javaBeanSerializer = (JavaBeanSerializer) serializer;
+
+            JSONObject json = new JSONObject();
+            try {
+                Map<String, Object> values = javaBeanSerializer.getFieldValuesMap(bean);
+                for (Map.Entry<String, Object> entry : values.entrySet()) {
+                    Object jsonValue = !deep ? entry.getValue() : beanToJson(entry.getValue(), config);
+                    json.put(entry.getKey(), jsonValue);
+                }
+            } catch (Exception e) {
+                throw new IllegalArgumentException(bean.getClass().getSimpleName() + " can't convert to map.", e);
+            }
+            return json;
+        }
+
+        throw new IllegalArgumentException(bean.getClass().getSimpleName() + " can't convert to map.");
     }
 
     private static Object beanToJson(Object bean, SerializeConfig config) {
@@ -232,24 +329,6 @@ public abstract class JsonTools {
             || Number.class.isAssignableFrom(clazz)
             || Date.class.isAssignableFrom(clazz);
         // @formatter:on
-    }
-
-    /**
-     * 将Java对象转换为Map
-     * 
-     * @param object Java对象
-     * @param clearBlankValue 是否清除空值
-     * @return Map
-     */
-    public static Map<String, Object> beanToMap(Object object, boolean clearBlankValue) {
-        if (object == null) {
-            return null;
-        }
-        Map<String, Object> map = (JSONObject) JSON.toJSON(object);
-        if (clearBlankValue) {
-            ConvertTools.clearBlankValue(map);
-        }
-        return map;
     }
 
     /**
