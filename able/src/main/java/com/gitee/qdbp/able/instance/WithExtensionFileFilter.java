18281070a0cda2627f25598301ea1cb23bce86d8
zhaohuihua <zhaohuihua@126.com>
2020-02-29 22:08:56 +0800

    FileFilter实现类

---
+++ able/src/main/java/com/gitee/qdbp/able/instance/WithExtensionFileFilter.java
@@ -0,0 +1,83 @@
+package com.gitee.qdbp.able.instance;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import com.gitee.qdbp.tools.files.PathTools;
+import com.gitee.qdbp.tools.utils.ConvertTools;
+import com.gitee.qdbp.tools.utils.StringTools;
+import com.gitee.qdbp.tools.utils.VerifyTools;
+
+/**
+ * 根据文件扩展名筛选文件的过滤器
+ *
+ * @author zhaohuihua
+ * @version 20200229
+ */
+public class WithExtensionFileFilter implements FileFilter, Serializable {
+
+    /** serialVersionUID **/
+    private static final long serialVersionUID = 1L;
+    /** 是否递归查询子文件夹 **/
+    private final boolean recursive = false;
+    /** 包含模式还是排序模式: true=包含, false=排除 **/
+    private final boolean include = true;
+    /** 文件扩展名映射表 **/
+    private final Map<String, Void> extensions;
+
+    /**
+     * 构造函数
+     * 
+     * @param extensions 文件扩展名列表
+     */
+    public WithExtensionFileFilter(String... extensions) {
+        this(false, true, extensions);
+    }
+
+    /**
+     * 构造函数
+     * 
+     * @param recursive 是否递归查询子文件夹
+     * @param extensions 文件扩展名列表
+     */
+    public WithExtensionFileFilter(boolean recursive, String... extensions) {
+        this(recursive, true, extensions);
+    }
+
+    /**
+     * 构造函数
+     * 
+     * @param recursive 是否递归查询子文件夹
+     * @param include 包含模式还是排序模式: true=包含, false=排除
+     * @param extensions 文件扩展名列表
+     */
+    public WithExtensionFileFilter(boolean recursive, boolean include, String... extensions) {
+        VerifyTools.requireNotBlank(extensions, "extensions");
+        this.extensions = new HashMap<>();
+        for (String extension : extensions) {
+            this.extensions.put(StringTools.removeLeft(extension.toLowerCase(), '.'), null);
+        }
+    }
+
+    @Override
+    public boolean accept(File file) {
+        if (file.isDirectory()) {
+            return recursive;
+        } else {
+            String extension = PathTools.getExtension(file.getName(), false);
+            if (extension == null) {
+                return false;
+            } else {
+                return include == this.extensions.containsKey(extension.toLowerCase());
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return (include ? "include:" : "exclude:") + ConvertTools.joinToString(extensions.keySet());
+    }
+
+}
